a1057a01a53a14a78a30d86ca22cb216cadb12aa
pzxm <892592558@qq.com>
2022-04-13 23:37:33 +0800

    fix: el-icon not working

--- src/plugins/element-plus.ts
+++ src/plugins/element-plus.ts
@@ -2,11 +2,16 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import { App } from 'vue'
+import * as ElIcons from '@element-plus/icons-vue'
 
 export default {
   install(app: App) {
     app.use(ElementPlus, {
       locale: zhCn
+    })
+    // 方式一
+    Object.keys(ElIcons).forEach((key) => {
+      app.component(key, ElIcons[key as keyof typeof ElIcons])
     })
   }
 }
