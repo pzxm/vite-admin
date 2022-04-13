a470862a96c7600db081ad2b2325a305df88c871
pzxm <892592558@qq.com>
2022-04-13 14:55:29 +0800

    chore: init vuex & path alias & scss

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,15 @@
 <template>
   <h1>首页</h1>
+  <p>{{ store.state.count }}</p>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useStore } from 'vuex'
+const store = useStore()
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+h1 {
+  color: $color;
+}
+</style>
